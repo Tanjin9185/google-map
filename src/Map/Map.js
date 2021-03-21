6f084220e417b26bfcba6a94d94d8961752308e8
Tanjin <tanjin.ahamed123@gmail.com>
2021-03-21 11:19:30 +0600

    need api key

---
+++ src/Map/Map.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+// import {API_KEY} from './config';
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: 23.71573,
+  lng: 90.38395
+};
+
+function Map() {
+  return (
+    <LoadScript
+      googleMapsApiKey="YOUR_API_KEY"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        <Marker      
+      position={center}
+    />
+    
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(Map)
